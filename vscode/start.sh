ace1aeb45daa0fcad5e5805fa911c7b128a871fe
Michal Charemza <michal@charemza.name>
2024-01-22 17:18:18 +0000

    feat: vscode via code-server
    Not quite ready to be merged - no Python packages and also installs minicode
    for arm since that's where I was testing it.

---
+++ vscode/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# When on EFS, we expect to not be able to change ownership, and we don't need to
+chown -R vscode:vscode /home/vscode
+
+set -e
+
+# Java programs can error if $HOSTNAME is not resolvable
+echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+
+export PGSSLROOTCERT=/certs/rds-global-bundle.pem
+
+sudo -E -H -u vscode code-server --bind-addr 0.0.0.0:8000 --auth none
