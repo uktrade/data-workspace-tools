2f8407b7c8adc3dd7499bdd046f483ebe0c58fc8
Michal Charemza <michal@charemza.name>
2024-02-21 18:29:44 +0000

    feat: disable dashboard from pgAdmin
    We see a lot of queries from pgAdmin's dashboard when looking at load on the
    database. It's hard to tell if they actually cause a problem, but turning them
    off by default should at least clear up a lot of noise.
    
    This includes an increase of the version of pgAdmin just by the nature of
    rebuilding with the latest version in PyPI. It's not pinned (it wasn't pinned
    before), but I suspect it increases the version from 7.8 to 8.3.

--- pgadmin/start.sh
+++ pgadmin/start.sh
@@ -11,6 +11,6 @@ chown -R pgadmin:pgadmin \
 	/var/lib/pgadmin \
 	/var/log/pgadmin
 
-python3 /usr/local/lib/python3.11/site-packages/pgadmin4/setup.py --load-servers /pgadmin4/servers.json
+python3 /usr/local/lib/python3.11/site-packages/pgadmin4/setup.py load-servers /pgadmin4/servers.json
 
 sudo -E -H -u pgadmin gunicorn --bind 0.0.0.0:8888 --workers=1 --threads=25 --chdir /usr/local/lib/python3.11/site-packages/pgadmin4 pgAdmin4:app
