a42f4d2bf3612e78f89ddb5fe9e5000c19aeb6e4
sekharpanja <sekhar.panja@digital.trade.gov.uk>
2024-11-29 16:22:18 +0000

    change the script to read whl files directly from s3 and upload index file

--- private_package_index/create_index_html.py
+++ private_package_index/create_index_html.py
@@ -1,13 +1,45 @@
 """
-This file creates a index html file locally with provided .whl file names
-so that this can be added to the root of package folder on s3.
+This file creates a html index file out of whl files available given path
+and uploads it to the root of package folder on s3.
 
-python3 create_index_html.py file1.whl file2.whl file3.whl
+python3 create_index_html.py s3://jupyter.notebook.uktrade.io/shared/ddat_packages/pypi/dwutils
 """
+import boto3
+import re
 import sys
 
+S3_BUCKET_NAME_REGEX = re.compile(r"^[a-z0-9-]{3,63}$")
 
-def create_index_html(whl_files):
+def is_s3_path(s3_path):
+    if not s3_path.startswith("s3://"):
+        return False
+    s3_path = s3_path[5:]
+    parts = s3_path.split("/", 1)
+    if len(parts) < 2:
+        return False
+
+    bucket_name = parts[1]
+    if not S3_BUCKET_NAME_REGEX.match(bucket_name):
+        return False
+
+    return True
+
+def split_s3_path(s3_path):
+    bucket = s3_path.split("/")[2]
+    prefix = s3_path.split(bucket)[1]
+    return bucket, prefix
+
+def get_wheel_filenames(s3, bucket, prefix):
+    filenames = []
+    result = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for item in result["Contents"]:
+        file = item["Key"]
+        print("file: ", file)
+        if file.lower().endswith(".whl"):
+            filenames.append(file)
+    return filenames
+
+def create_and_upload_index_html(s3, bucket, prefix, whl_files):
     html_str = (
         "<!DOCTYPE html>"
         + "<html>"
@@ -15,20 +47,29 @@ def create_index_html(whl_files):
         + "".join(
             [
                 f'<a href="{filename}">{filename}</a>'
-                for filename in whl_files if filename.lower().endswith('.whl')
+                for filename in whl_files
             ]
         )
         + "</body>"
         + "</html>"
     )
-    with open("index.html", "w") as html_file:
-        html_file.write(html_str)
+    s3.put_object(
+        Body=html_str,
+        Bucket=bucket,
+        ContentType="text/html",
+        Key=prefix
+    )
+    print("upload finished")
 
+if len(sys.argv) <= 1:
+    print("No s3 path found.")
 
-whl_files = []
-if len(sys.argv) > 1:
-    whl_files = sys.argv[1:]
-    print("Wheel files list:", whl_files)
-    create_index_html(whl_files)
-else:
-    print("No wheel files found.")
+s3_client = boto3.client("s3")
+s3_path = sys.argv[1]
+if not is_s3_path(s3_path):
+    print("not valid s3 path")
+bucket, prefix = split_s3_path(s3_path)
+print(f"looking for whl files in {bucket} with prefix {prefix}")
+whl_files = get_wheel_filenames(s3_client, bucket, prefix)
+print("whl files:", whl_files)
+create_and_upload_index_html(s3_client, bucket, prefix, whl_files)
