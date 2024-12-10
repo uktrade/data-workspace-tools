cf098abb10de901cc064752cffd86f4fe7b42b0a
sekharpanja <sekhar.panja@digital.trade.gov.uk>
2024-12-10 13:23:34 +0000

    update gitlab private pypi index script file, add more logging

--- private_package_index/create_index_html.py
+++ private_package_index/create_index_html.py
@@ -12,16 +12,20 @@ S3_BUCKET_NAME_REGEX = re.compile(r"^[a-z0-9-]{3,63}$")
 
 def is_s3_path(s3_path):
     if not s3_path.startswith("s3://"):
+        print("doesn't start with s3://")
         return False
     s3_path = s3_path[5:]
     parts = s3_path.split("/", 1)
     if len(parts) < 2:
+        print("not enough parts in the path")
         return False
 
     bucket_name = parts[0]
     if not S3_BUCKET_NAME_REGEX.match(bucket_name):
+        print("failed regex check")
         return False
 
+    print("all good with the path")
     return True
 
 def split_s3_path(s3_path):
@@ -32,11 +36,14 @@ def split_s3_path(s3_path):
 def get_wheel_filenames(s3, bucket, prefix):
     filenames = []
     result = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    print(result)
     if 'Contents' in result:
         for item in result["Contents"]:
             file = item["Key"]
             if file.lower().endswith(".whl"):
                 filenames.append(file)
+    if len(filenames) == 0:
+        raise ValueError("no whl files found")
     return filenames
 
 def create_and_upload_index_html(s3, bucket, prefix, whl_files):
@@ -64,16 +71,14 @@ def create_and_upload_index_html(s3, bucket, prefix, whl_files):
 if len(sys.argv) <= 1:
     print("No s3 path found.")
 
-s3_client = boto3.client("s3")
+s3_client = boto3.client("s3", region_name="eu-west-2")
 s3_path = sys.argv[1]
 if not is_s3_path(s3_path):
-    print("not valid s3 path")
+    raise ValueError("not valid s3 path")
 bucket, prefix = split_s3_path(s3_path)
 print(f"looking for whl files in {bucket} with prefix {prefix}")
 whl_files = get_wheel_filenames(s3_client, bucket, prefix)
-if whl_files:
-    print("whl files found:", whl_files)
-    print("creating and uploading index")
-    create_and_upload_index_html(s3_client, bucket, prefix, whl_files)
-else:
-    print("no whl files to create index")
+print("whl files found:", whl_files)
+print("creating and uploading index")
+create_and_upload_index_html(s3_client, bucket, prefix, whl_files)
+print("index upload finished!")
